d02423007bbabe4ae6eeadf3e0120eef38a841a1
Agustin Petrini <agustin@sid-8.com>
2025-10-28 18:55:11 +0900

    Implement printing all placeholder commands

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,18 @@ use processing::Command;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     match input::validate_args()? {
+        Command::Add (_, profile) =>
+            println!("ADD {}", profile),
+        Command::Remove(_, profile) =>
+            println!("REMOVE {}", profile),
         Command::Sync (profile_syncs) =>
             for profile_sync in profile_syncs {
-                println!("src: {} tgt: {}", profile_sync.source.display(), profile_sync.target.display());
+                println!("SYNC src: {} tgt: {}", profile_sync.source.display(), profile_sync.target.display());
             }
         Command::List (profiles) =>
             for profile in profiles {
                 println!("{}", profile);
             }
-        _ => println!("Input validated"),
     }
 
     Ok(())
